40101454ca8b8422ce7fad7425f7a0191be1464e
Marios Giatsidis <giatsidis@addc.network.arxnet>
2022-06-28 13:19:34 +0300

    ver 2.0

---
+++ js/710-legacy.17e3681b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunklottery_app"]=self["webpackChunklottery_app"]||[]).push([[710],{4710:function(t,e,n){n.r(e),n.d(e,{default:function(){return f}});var l=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"flex justify-center mt-8"},[t._v(" page does not found :) ")])},u=[],r={},s=r,a=n(1001),c=(0,a.Z)(s,l,u,!1,null,null,null),f=c.exports}}]);
+//# sourceMappingURL=710-legacy.17e3681b.js.map
